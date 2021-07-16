8bdf5cce122b541a2d0092e8305de4a8eea2ff10
reut07 <reut110488>
2021-07-16 19:54:19 +0300

    The first task is completed

---
+++ src/main/java/by/ereut/senlacourses/task1/DuplicateSymbolCounter.java
@@ -0,0 +1,23 @@
+package by.ereut.senlacourses.task1;
+
+import lombok.AllArgsConstructor;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@AllArgsConstructor
+public class DuplicateSymbolCounter {
+
+    private static final String regExp = "(.)\\1{2}";
+    private String enteredString;
+
+    public int countMatches() {
+        int value = 0;
+        final Matcher matcher = Pattern.compile(regExp).matcher(enteredString);
+        while(matcher.find()) {
+            value++;
+        }
+        return value;
+    }
+
+}
---
+++ src/main/java/by/ereut/senlacourses/task1/Runner.java
@@ -0,0 +1,15 @@
+package by.ereut.senlacourses.task1;
+
+import java.util.Scanner;
+
+public class Runner {
+
+    public static void main(String[] args) {
+        System.out.println("Please, enter the string");
+        Scanner scanner = new Scanner(System.in);
+        final String enteredString = scanner.nextLine();
+        final DuplicateSymbolCounter counter = new DuplicateSymbolCounter(enteredString);
+        System.out.print(counter.countMatches());
+
+    }
+}
